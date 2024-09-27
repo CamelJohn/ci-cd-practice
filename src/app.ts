19c7c7adf1f0c9d50aaf21cdbc78b473f358a285
= <=>
2024-09-27 04:34:59 +0300

    Add formatting

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import middleware from './middleware';
+import main_router from './router';
 
 const webApp = express();
 
@@ -13,9 +14,8 @@ webApp.use(helmet());
 webApp.use(morgan('dev'));
 
 webApp.use('/health-check', middleware.health_check);
+webApp.use('/api/v1/', main_router)
 webApp.use('*', middleware.catch_all);
 webApp.use(middleware.error_handler);
-
-// webApp.listen(3000, () => console.info('Server started listening on port', 3000));
 
 export default webApp;
--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -5,17 +5,17 @@ import httpWrapper from 'supertest';
 describe('Web app should satisfy following:', () => {
     beforeEach(async () => start(3000));
     afterEach(async () => stop());
-   const http = httpWrapper(webApp);
+    const http = httpWrapper(webApp);
 
-   it('handles non existent routes', async () => {
-       const response = await http.get('/non-existent-route');
-       expect(response.status).toBe(404);
-       expect(response.body.message).toBe('Not Found');
-   });
+    it('handles non existent routes', async () => {
+        const response = await http.get('/non-existent-route');
+        expect(response.status).toBe(404);
+        expect(response.body.message).toBe('Not Found');
+    });
 
-   it('responds to health checks', async () => {
-       const response = await http.get('/health-check');
-       expect(response.status).toBe(200);
-       expect(response.body.message).toBe('OK');
-   })
+    it('responds to health checks', async () => {
+        const response = await http.get('/health-check');
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('OK');
+    });
 });
